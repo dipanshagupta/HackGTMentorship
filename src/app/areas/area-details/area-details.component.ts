2a1cdd614359138250a6d33863fd984bb7f7df1e
Dipansha <dipansha@Dipanshas-MacBook-Pro.local>
2017-10-14 12:28:54 -0400

    Adding areas ts

---
+++ src/app/areas/area-details/area-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-area-details',
+  templateUrl: './area-details.component.html',
+  styleUrls: ['./area-details.component.css']
+})
+export class AreaDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
