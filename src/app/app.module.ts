90b6ddf80c46a73f14559bc95c0f9546ad9bc67d
Dipansha <dipansha@Dipanshas-MacBook-Pro.local>
2017-10-14 12:15:43 -0400

    Adding person, mentor and student classes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StudentDetailsComponent } from './students/student-details/student-details.component';
+import { StudentListComponent } from './students/student-list/student-list.component';
+import { PersonDetailsComponent } from './persons/person-details/person-details.component';
+import { PersonListComponent } from './persons/person-list/person-list.component';
+import { MentorDetailsComponent } from './mentors/mentor-details/mentor-details.component';
+import { MentorListComponent } from './mentors/mentor-list/mentor-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentDetailsComponent,
+    StudentListComponent,
+    PersonDetailsComponent,
+    PersonListComponent,
+    MentorDetailsComponent,
+    MentorListComponent
   ],
   imports: [
     BrowserModule
