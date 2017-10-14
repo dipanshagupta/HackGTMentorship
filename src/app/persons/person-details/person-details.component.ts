c6290f60d1b5a26fc787fcf17db9d403856ba27f
asinsinwal <assinsin@ncsu.edu>
2017-10-14 13:30:31 -0400

    Handled Person JSON interaction

--- src/app/persons/person-details/person-details.component.ts
+++ src/app/persons/person-details/person-details.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Person } from '../person';
+import { PersonService } from '../person.service';
 
 @Component({
   selector: 'app-person-details',
   templateUrl: './person-details.component.html',
   styleUrls: ['./person-details.component.css']
 })
-export class PersonDetailsComponent implements OnInit {
+export class PersonDetailsComponent {
+  @Input()
+  person: Person;
 
-  constructor() { }
+  @Input()
+  createHandler: Function;
+  @Input()
+  updateHandler: Function;
+  @Input()
+  deleteHandler: Function;
 
-  ngOnInit() {
+  constructor (private personService: PersonService) {}
+
+  createPerson(person: Person) {
+    this.personService.createPerson(person).then((newPerson: Person) => {
+      this.createHandler(newPerson);
+    });
   }
 
+  updatePerson(person: Person): void {
+    this.personService.updatePerson(person).then((updatedPerson: Person) => {
+      this.updateHandler(updatedPerson);
+    });
+  }
+
+  deletePerson(personId: String): void {
+    this.personService.deletePerson(personId).then((deletedPersonId: String) => {
+      this.deleteHandler(deletedPersonId);
+    });
+  }
 }
--- src/app/persons/person-list/person-list.component.ts
+++ src/app/persons/person-list/person-list.component.ts
@@ -1,15 +1,88 @@
 import { Component, OnInit } from '@angular/core';
+import { Person } from '../person';
+import { PersonService } from '../person.service';
+import { PersonDetailsComponent } from '../person-details/person-details.component';
+
 
 @Component({
   selector: 'app-person-list',
   templateUrl: './person-list.component.html',
-  styleUrls: ['./person-list.component.css']
+  styleUrls: ['./person-list.component.css'],
+  providers: [PersonService]
 })
 export class PersonListComponent implements OnInit {
 
-  constructor() { }
+  persons: Person[]
+  selectedPerson: Person
+
+  constructor(private personService: PersonService) { }
 
   ngOnInit() {
+     this.personService
+      .getPersons()
+      .then((persons: Person[]) => {
+        this.persons = persons.map((person) => {
+          if (!person.phone) {
+            person.phone = {
+              mobile: '',
+              work: ''
+            }
+          }
+          return person;
+        });
+      });
+  }
+
+  private getIndexOfPerson = (personId: String) => {
+    return this.persons.findIndex((person) => {
+      return person._id === personId;
+    });
+  }
+
+  selectPerson(person: Person) {
+    this.selectedPerson = person
+  }
+
+  createNewPerson() {
+    var person: Person = {
+      name: '',
+      email: '',
+      linkedin: '',
+      twitter: '',
+      role: '',
+  
+      interest: [{area_id: ''}],
+  
+      university: '',
+      password: ''
+    };
+
+    // By default, a newly-created person will have the selected state.
+    this.selectPerson(person);
+  }
+
+  deletePerson = (personId: String) => {
+    var idx = this.getIndexOfPerson(personId);
+    if (idx !== -1) {
+      this.persons.splice(idx, 1);
+      this.selectPerson(null);
+    }
+    return this.persons;
+  }
+
+  addPerson = (person: Person) => {
+    this.persons.push(person);
+    this.selectPerson(person);
+    return this.persons;
+  }
+
+  updatePerson = (person: Person) => {
+    var idx = this.getIndexOfPerson(person._id);
+    if (idx !== -1) {
+      this.persons[idx] = person;
+      this.selectPerson(person);
+    }
+    return this.persons;
   }
 
 }
